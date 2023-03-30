a5ffadb41a642880b3a1517577e1bff085b1dadb
Matias <64340009+CineraM@users.noreply.github.com>
2023-03-30 02:01:06 -0400

    faster testing

--- testing_script.py
+++ testing_script.py
@@ -15,69 +15,51 @@ import numpy as np
 
 # Change the path of the folders if needed
 test_img_folder =       '/home/matias/Documents/nn/test'
-base_model_path =       '/home/matias/Documents/GitHub/food-state-classification/project1_part2.tf'
-inception_model_path =  '/home/matias/Documents/GitHub/food-state-classification/project1_part2_inception.tf'
-resnet_model_path =     '/home/matias/Documents/GitHub/food-state-classification/project1_part2_resnet.tf'
+base_model_path =       '/home/matias/Documents/cnn_saved_models/project1_part2.tf'
+inception_model_path =  '/home/matias/Documents/cnn_saved_models/project1_part2_inception.tf'
+resnet_model_path =     '/home/matias/Documents/cnn_saved_models/project1_part2_resnet.tf'
 
 class_names = ['creamy_paste', 'diced', 'floured', 'grated', 'juiced', 
 'jullienne', 'mixed', 'other', 'peeled', 'sliced', 'whole']
 IMG_SIZE=264
 
 # load the trained model
-def load_model(filename):
+def loadModel(filename):
     return tf.keras.models.load_model(filename)
 
+def predictModel(model):
+    img_batch = []
+    for img in os.listdir(test_img_folder):
+        img_path = os.path.join(test_img_folder, img)
+        read_img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
 
-#############################################################################
+        img_array = image.img_to_array(read_img)
+        img_array = np.expand_dims(img_array, axis=0)
+
+        img_batch.append(img_array)
+    images = np.vstack(img_batch)
+    return model.predict(images, verbose=0)
+
+
 print("Predicting Base Model...")
-model = load_model(base_model_path)
+model = loadModel(base_model_path)
+base_results = predictModel(model)
 
-base_results = []
-for img in os.listdir(test_img_folder):
-    img_path = os.path.join(test_img_folder, img)
-    read_img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
 
-    img_array = image.img_to_array(read_img)
-    img_batch = np.expand_dims(img_array, axis=0)
-
-    prediction = model.predict(img_batch, verbose=0)
-    base_results.append(prediction[0])
-
-#############################################################################
 print("Predicting Inception Model...")
-model = load_model(inception_model_path)
+model = loadModel(inception_model_path)
+inception_results = predictModel(model)
 
-inception_results = []
-for img in os.listdir(test_img_folder):
-    img_path = os.path.join(test_img_folder, img)
-    read_img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
-
-    img_array = image.img_to_array(read_img)
-    img_batch = np.expand_dims(img_array, axis=0)
-
-    prediction = model.predict(img_batch, verbose=0)
-    inception_results.append(prediction[0])
-
-#############################################################################
 print("Predicting Resnet Model...")
-model = load_model(resnet_model_path)
+model = loadModel(resnet_model_path)
+resnet_results = predictModel(model)
 
-resnet_results = []
-for img in os.listdir(test_img_folder):
-    img_path = os.path.join(test_img_folder, img)
-    read_img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
-
-    img_array = image.img_to_array(read_img)
-    img_batch = np.expand_dims(img_array, axis=0)
-
-    prediction = model.predict(img_batch, verbose=0)
-    resnet_results.append(prediction[0])
-
-#############################################################################
+print(len(base_results), len(inception_results), len(resnet_results))
 # sum the results and take the average
 ensemble_results = np.array(base_results) + np.array(base_results) + np.array(resnet_results)
 ensemble_results/=3
 
+# picking the higest prediciton and assigning it a class based on idex
 results = []
 index = 0
 for img in os.listdir(test_img_folder):
@@ -89,7 +71,8 @@ results_dict = dict(results)
 with open('prediction.json', 'w') as fp:
     json.dump(results_dict, fp, indent=4,separators=(',', ':'))
 
-print("results.json created")
+print("Ensemble prediction stored in 'results.json' ")
+
 
 # ---- This code calculates the accuracy of the prediction based on ground truth ----
 # Ground truth files, and the results of my network. 
